3c0cb050f09d949de7b2609a48558c6dc9c5889b
Briana Lee <L00126866@student.lyit.ie>
2018-03-28 21:51:27 +0100

    Cancel Booking Page

--- cancelBooking.php
+++ cancelBooking.php
@@ -1,35 +1,37 @@
+<?php include('process.php'); ?>
 <html>
 <head>
 <title>Applied Team 4 | Golf Booking System</title>
+  <!-- scripts -->
 <script src="jquery-3.3.1.min.js"></script>
-<link rel="stylesheet" href="css/style.css">
+<script src="script.js"></script>
+  <link rel="stylesheet" href="css/cancelStyle.css">
 </head>
 <body>
-<div id="floating_div">
-    <h2> Cancel Tee Booking </h2>
-          <table class=bookingConfirmation>
-            <tr>  
-            <td><label>Booking Reference No.:</label></td>
-            <td><input type="text"/></td>
-            </tr>
-            <tr>  
-            <td><label>Date:</label></td>
-            <td><input id="date" type="text"></td>
-            </tr>
-            <tr>  
-            <td><label>Time:</label></td>
-            <td><input id="time" type="text"></td>
-            </tr>
-            <tr>  
-            <td><label>Booking Name:</label></td>
-            <td><input id="bookingName" type="text"></td>
-            </tr>
-            <tr>
-            <td><button class="backButton"><a href="visitors.php">Back</a></td>
-            <td><button class="confirmButton"><a href="cancelConfirmation.php">Cancel Booking</a></td>
-            </tr>
-          </table>
-          </form>
-</div>
+ <form id="cancel_form">
+      <h1>Cancel Booking</h1>
+      <div align="center"><p>Please enter your booking details below.</p></div>
+      <div id="error_msg"></div>
+	  <div>
+	 	<input type="text" name="bookingRef" placeholder="Booking Ref. Number" id="bookingRef" >
+	    <span align="center"></span>
+	  </div>
+	  <div>
+	    <input type="date" name="bookingDate" placeholder="Date" id="bookingDate">
+		<span align="center"></span>
+	  </div>
+	  <div>
+	   <input type="text" name="bookingTime" placeholder="Booking Time" id="bookingTime">
+	   <span align="center"></span>
+	  </div>
+	  	  <div>
+	   <input type="text" name="bookingName" placeholder="Booking Name" id="bookingName">
+	   <span align="center"></span>
+	  </div>
+	  <div>
+	 	<button type="button" name="cancel" id="cancel_btn">Cancel</button>
+	 	<button type="button" name="back" id="back_btn">Back</button>
+	  </div>
+	</form>
 </body>
 </html>
