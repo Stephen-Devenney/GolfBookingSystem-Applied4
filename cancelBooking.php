5f4ebae28cc2c65a928d73534252c9de5be1e426
Briana Lee <L00126866@student.lyit.ie>
2018-03-28 22:00:51 +0100

    Cancel Booking Page

---
+++ cancelBooking.php
@@ -0,0 +1,37 @@
+<?php include('process.php'); ?>
+<html>
+<head>
+<title>Applied Team 4 | Golf Booking System</title>
+  <!-- scripts -->
+<script src="jquery-3.3.1.min.js"></script>
+<script src="script.js"></script>
+  <link rel="stylesheet" href="css/cancelStyle.css">
+</head>
+<body>
+ <form id="cancel_form">
+      <h1>Cancel Booking</h1>
+      <div align="center"><p>Please enter your booking details below.</p></div>
+      <div id="error_msg"></div>
+	  <div>
+	 	<input type="text" name="bookingRef" placeholder="Booking Ref. Number" id="bookingRef" >
+	    <span align="center"></span>
+	  </div>
+	  <div>
+	    <input type="date" name="bookingDate" placeholder="Date" id="bookingDate">
+		<span align="center"></span>
+	  </div>
+	  <div>
+	   <input type="text" name="bookingTime" placeholder="Booking Time" id="bookingTime">
+	   <span align="center"></span>
+	  </div>
+	  	  <div>
+	   <input type="text" name="bookingName" placeholder="Booking Name" id="bookingName">
+	   <span align="center"></span>
+	  </div>
+	  <div>
+	 	<button type="button" name="cancel" id="cancel_btn">Cancel</button>
+	 	<button type="button" name="back" id="back_btn">Back</button>
+	  </div>
+	</form>
+</body>
+</html>
