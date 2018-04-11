440218f75aca33e0b770e9d24a3eaddd7d587d4f
Irwin Dylan <L00118174@lyitnet.lyit.ie>
2018-04-11 09:49:26 +0100

    cleaned up visitors.php

--- visitors.php
+++ visitors.php
@@ -2,15 +2,14 @@
 <head>
 <title>Applied Team 4 | Golf Booking System</title>
 <script src="jquery-3.3.1.min.js"></script>
-<script src="login.js"></script>
 <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div id="floating_div">
     <h2> Visitor Bookings </h2>
-    <center><button class="bookTeeTime"><a href="visitorBooking.php">Book A Tee Time</button></center>
-    <center><button class="cancelBooking"><a href="cancelBooking.php">Cancel Booking</button><center>
-    <button class="back"><a href ="teetimes.html">Return</a></button>
+    <center><button class="bookTeeTime"><a href="visitorBooking.php">Book A Tee Time</a></button></center>
+    <center><button class="cancelBooking"><a href="cancelBooking.php">Cancel Booking</a></button></center>
+    <center><button class="back"><a href ="teetimes.html">Return</a></button></center>
   </div>
 </div>
 </body>
